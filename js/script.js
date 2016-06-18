4a2769d15a5af09bcb24cf1df3f25e1a5f82f26b
sselea <sselea88@gmail.com>
2016-06-18 13:07:07 -0400

    mobile friendly

--- js/script.js
+++ js/script.js
@@ -122,8 +122,8 @@ $(document).ready(function() {
 
 
         function introAnim() {
-            $(".name-first").show("blind", 2000) && $(".name-last").show("drop", 2500);
-            $(".enter-btn").delay(3000).fadeIn(1000);
+            $(".name-first").show("blind", 1000) && $(".name-last").show("drop", 1500);
+            $(".enter-btn").delay(1000).fadeIn(1000);
         }
 
         if (anim == true) {
@@ -257,7 +257,6 @@ $(document).ready(function() {
     function startwm() {
 
         console.log(anim);
-        //Setting witdth of the title - Animation
         function wanderAnim() {
             console.log("wander anim");
             var spanWidth = $('.wm span').width();
@@ -284,25 +283,9 @@ $(document).ready(function() {
             wanderAnim();
         }
 
-
-        // Making sure no other pages show when you resize window.
-        // Definitely shouldn't be here
-
-
         sliderLoop("wander");
 
     };
-
-
-    //Enter button for scrolling to WanderMunch
-    // Shouldn't be here
-
-
-    // Scroll Up in WanderMunch
-
-
-    //Making sure animations in Reddit don't run the second time.
-    // Bypassing the startredd function and simple scrolling down.
 
 
     function startredd() {
@@ -346,22 +329,6 @@ $(document).ready(function() {
         if (anim == true ) {
             redditAnim();
         }
-
-
-
-        //Description, Tile and go-to button animation and
-
-
-
-
-
-
-
-
-
-
-
-        // Reddit image slider - again. Need to put into a function
 
         $('.show-slider ').mouseover(function() {
             $('.redd-desc').animate({
@@ -493,72 +460,3 @@ $.fn.rotate = function(options) {
 
     return $this;
 };
-
-(function($){
-
-    /**
-     * Copyright 2012, Digital Fusion
-     * Licensed under the MIT license.
-     * http://teamdf.com/jquery-plugins/license/
-     *
-     * @author Sam Sehnert
-     * @desc A small plugin that checks whether elements are within
-     *       the user visible viewport of a web browser.
-     *       only accounts for vertical position, not horizontal.
-     */
-    var $w = $(window);
-    $.fn.visible = function(partial,hidden,direction){
-
-        if (this.length < 1)
-            return;
-
-        var $t        = this.length > 1 ? this.eq(0) : this,
-            t         = $t.get(0),
-            vpWidth   = $w.width(),
-            vpHeight  = $w.height(),
-            direction = (direction) ? direction : 'both',
-            clientSize = hidden === true ? t.offsetWidth * t.offsetHeight : true;
-
-        if (typeof t.getBoundingClientRect === 'function'){
-
-            // Use this native browser method, if available.
-            var rec = t.getBoundingClientRect(),
-                tViz = rec.top    >= 0 && rec.top    <  vpHeight,
-                bViz = rec.bottom >  0 && rec.bottom <= vpHeight,
-                lViz = rec.left   >= 0 && rec.left   <  vpWidth,
-                rViz = rec.right  >  0 && rec.right  <= vpWidth,
-                vVisible   = partial ? tViz || bViz : tViz && bViz,
-                hVisible   = partial ? lViz || rViz : lViz && rViz;
-
-            if(direction === 'both')
-                return clientSize && vVisible && hVisible;
-            else if(direction === 'vertical')
-                return clientSize && vVisible;
-            else if(direction === 'horizontal')
-                return clientSize && hVisible;
-        } else {
-
-            var viewTop         = $w.scrollTop(),
-                viewBottom      = viewTop + vpHeight,
-                viewLeft        = $w.scrollLeft(),
-                viewRight       = viewLeft + vpWidth,
-                offset          = $t.offset(),
-                _top            = offset.top,
-                _bottom         = _top + $t.height(),
-                _left           = offset.left,
-                _right          = _left + $t.width(),
-                compareTop      = partial === true ? _bottom : _top,
-                compareBottom   = partial === true ? _top : _bottom,
-                compareLeft     = partial === true ? _right : _left,
-                compareRight    = partial === true ? _left : _right;
-
-            if(direction === 'both')
-                return !!clientSize && ((compareBottom <= viewBottom) && (compareTop >= viewTop)) && ((compareRight <= viewRight) && (compareLeft >= viewLeft));
-            else if(direction === 'vertical')
-                return !!clientSize && ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-            else if(direction === 'horizontal')
-                return !!clientSize && ((compareRight <= viewRight) && (compareLeft >= viewLeft));
-        }
-    };
-
-})(jQuery);
